8dfdee209c8c1843a79fd67afa6b0a2e5080bcd2
Luke Nihlen <luke.nihlen@gmail.com>
2021-07-23 21:56:19 -0400

    adding a few thoughts about stack setup

--- src/Function.cpp
+++ src/Function.cpp
@@ -23,19 +23,29 @@ bool Function::buildEntryTrampoline(LighteningJIT* jit) {
     // in order on the stack starting with argument 0 at SP + sizeof(void*) for the calling address. Each Slot is
     // 16 bytes, so stack looks like: (higher values of stack address at top)
 
-    // ARG N-1
-    // ARG N-2                       SP + WordSize + ((N -2) * 16)
+    // Keyword/Arg Slot pairs
+    // Number of Keyword/Arg pairs: word size
+    // Number of Ordered Args: word size
+    // Hash of Target type: 8 bytes (ignored after dispatch)
+    // Hash of Selector name: 8 bytes (ignored after dispatch)
+    // Return Value Slot             SP + AddressSize - will be at the top of the stack after return
+    // Caller Return Address ======= SP
+    // ARG 0                         SP - AddressSize
+    // ARG 1                         SP - AddressSize - 16
     // ....
-    // ARG 0                         SP + WordSize
-    // Return Value Slot             SP + 16 - will be at the top of the stack after return
-    // Caller Return Address ======= SP + 0
-    // Virtual Reg Spill 0           SP - WordSize
-    // Virtual Reg Spill 1           SP - (WordSize * 2)
+    // ARG N-1                       SP - AddressSize - (16 * (N -1))
+
+    // <--- start of scratch space for callees
+    // Virtual Reg Spill 0
+    // Virtual Reg Spill 1
+    // ...
 
     // On the caller side, there will be a count and list of ordered args, then a count and list of keyword/arg pairs.
     // Calling from within Hadron ABI, there's a part where we do dispatch using target and selector hashes to find
     // this function object, then we have the same entry conditions as here, which is that there are the two lists
     // of arguments and their counts available somewhere.
+
+
 
     jit->leaveABI(stackSize);
 }
