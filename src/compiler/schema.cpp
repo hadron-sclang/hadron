88cdddc22978d3d67484a91eec48de28e82ee999
Luke Nihlen <luke.nihlen@gmail.com>
2021-09-23 18:38:09 -0400

    adds a little code to schema

--- src/compiler/schema.cpp
+++ src/compiler/schema.cpp
@@ -1,15 +1,18 @@
 // schema, parses a SuperCollider input class file and generates a Schema C++ output file
 #include "hadron/ErrorReporter.hpp"
 #include "hadron/Lexer.hpp"
+#include "hadron/Parser.hpp"
 #include "hadron/SourceFile.hpp"
 
 #include "gflags/gflags.h"
 
+#include <fstream>
 #include <iostream>
 #include <memory>
+#include <string>
 
 DEFINE_string(classFile, "", "Path to the SC class file to generate schema file from.");
-DEFINE_string(schemaFile, "", "Path to save the schema output file to.");
+DEFINE_string(schemaFile, "", "Path to save the schema output header file to.");
 
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, false);
@@ -19,7 +22,39 @@ int main(int argc, char* argv[]) {
         std::cerr << "schema parser failed to read input class file: " << FLAGS_classFile << std::endl;
         return -1;
     }
-//    Lexer lexer()
+    auto code = sourceFile.codeView();
+    errorReporter->setCode(code.data());
+
+    hadron::Lexer lexer(code, errorReporter);
+    if (!lexer.lex() || !errorReporter->ok()) {
+        std::cerr << "schema failed to lex input class file: " << FLAGS_classFile << std::endl;
+        return -1;
+    }
+
+    hadron::Parser parser(&lexer, errorReporter);
+    if (!parser.parseClass() || !errorReporter->ok()) {
+        std::cerr << "schema failed to parse input class file: " << FLAGS_classFile << std::endl;
+        return -1;
+    }
+
+    std::ofstream outFile(FLAGS_schemaFile);
+    if (!outFile) {
+        std::cerr << "schema file error on ouput file: " << FLAGS_schemaFile << std::endl;
+    }
+
+    const hadron::parse::Node* node = parser.root();
+    while (node) {
+        if (node->nodeType != hadron::parse::NodeType::kClass) {
+            std::cerr << "schema did not find a Class root node in parse tree for file: " << FLAGS_classFile
+                    << std::endl;
+        }
+        const hadron::parse::ClassNode* classNode = reinterpret_cast<const hadron::parse::ClassNode*>(node);
+        auto token = lexer.tokens()[classNode->tokenIndex];
+        std::string className(token.range.data(), token.range.size());
+        std::cout << className << std::endl;
+
+        node = classNode->next.get();
+    }
 
     return 0;
 }
