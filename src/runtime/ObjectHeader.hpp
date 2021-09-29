47f31e03c3f2e23be422dd62a1ffedec79f285f5
Luke Nihlen <luke.nihlen@gmail.com>
2021-09-29 19:07:15 -0400

    object header

---
+++ src/runtime/ObjectHeader.hpp
@@ -0,0 +1,10 @@
+#ifndef SRC_RUNTIME_OBJECT_HEADER_HPP_
+#define SRC_RUNTIME_OBJECT_HEADER_HPP_
+
+namespace runtime {
+
+// Object instances in Hadron are contiguous blocks of Slots.
+
+} // namespace runtime
+
+#endif // SRC_RUNTIME_OBJECT_HEADER_HPP_
