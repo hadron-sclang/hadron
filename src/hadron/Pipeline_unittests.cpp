4130e3f94cdc8d98434aaa962f08ae96d656dce6
Luke Nihlen <luke.nihlen@gmail.com>
2022-01-12 14:24:32 -0800

    Update Lightening to include bugfix (#68)

--- src/hadron/Pipeline_unittests.cpp
+++ src/hadron/Pipeline_unittests.cpp
@@ -35,26 +35,21 @@ TEST_CASE_FIXTURE(PipelineTestFixture, "Pipeline") {
 
     SUBCASE("nil block") {
         Pipeline p;
-        // TODO: see https://github.com/hadron-sclang/hadron/issues/66, once resolved remove the VM setting.
-        p.setJitToVirtualMachine(true);
         REQUIRE_NE(p.compileCode(context(), "nil"), Slot());
     }
 
     SUBCASE("this call") {
         Pipeline p;
-        p.setJitToVirtualMachine(true);
         REQUIRE_NE(p.compileCode(context(), "this.primitiveFailed"), Slot());
     }
 
     SUBCASE("call chaining") {
         Pipeline p;
-        p.setJitToVirtualMachine(true);
         REQUIRE_NE(p.compileCode(context(), "this.asString.post"), Slot());
     }
 
     SUBCASE("simple if block") {
         Pipeline p;
-        p.setJitToVirtualMachine(true);
         REQUIRE_NE(p.compileCode(context(), "if(true,{0})"), Slot());
     }
 }
