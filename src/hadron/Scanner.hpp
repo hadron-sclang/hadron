a2a1fe54698e47661fde7938d61a2247e48d980a
Luke Nihlen <luke.nihlen@gmail.com>
2022-04-30 22:00:06 -0400

    add the Scanner

---
+++ src/hadron/Scanner.hpp
@@ -0,0 +1,12 @@
+#ifndef SRC_HADRON_SCANNER_HPP_
+#define SRC_HADRON_SCANNER_HPP_
+
+namespace hadron {
+
+template<typename T>
+class Scanner {
+};
+
+} // namespace hadron
+
+#endif // SRC_HADRON_SCANNER_HPP_
