1b061036f5990a40b2a1b8aa1c2532c9ce9b036c
Luke Nihlen <luke.nihlen@gmail.com>
2022-08-02 08:22:31 -0400

    more wip on hlang-test

--- src/hlang-test.cpp
+++ src/hlang-test.cpp
@@ -1,26 +1,33 @@
 // hlang, command line SuperCollider language script interpreter
 #include "hadron/ErrorReporter.hpp"
+#include "hadron/internal/FileSystem.hpp"
 #include "hadron/Runtime.hpp"
+#include "hadron/SourceFile.hpp"
 
 #include "gflags/gflags.h"
 #include "spdlog/spdlog.h"
 
 #include <iostream>
 #include <memory>
-
-DEFINE_string(sourceFile, "", "Path to the test code file to execute.");
+#include <string>
 
 int main(int argc, char* argv[]) {
-    gflags::ParseCommandLineFlags(&argc, &argv, false);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     spdlog::default_logger()->set_level(spdlog::level::trace);
+
+    if (argc != 2) {
+        std::cerr << "usage: hlang-test [options] input_file.sctest" << std::endl;
+        return -1;
+    }
 
     auto errorReporter = std::make_shared<hadron::ErrorReporter>();
     hadron::Runtime runtime(errorReporter);
     if (!runtime.initInterpreter()) { return -1; }
 
-    fs::path sourcePath(FLAGS_sourceFile);
-
+    fs::path sourcePath(argv[1]);
+    auto sourceFile = hadron::SourceFile(sourcePath.string());
+    if (!sourceFile.read(errorReporter)) { return -1; }
 
     return 0;
 }
