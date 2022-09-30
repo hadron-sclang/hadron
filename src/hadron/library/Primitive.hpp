cc93ea0e7b37bd1cb4989e7ad66a2cfaed2d6c53
Luke Nihlen <luke.nihlen@gmail.com>
2022-09-29 22:22:08 -0400

    more roughing out

--- src/hadron/library/Primitive.hpp
+++ src/hadron/library/Primitive.hpp
@@ -20,6 +20,14 @@ SCMethod* bindPrimitive(void* functionPointer) {
     funcNode->setArg(1, framePointerReg);
     asmjit::x86::Gp stackPointerReg = compiler.newGp(asmjit::TypeId::kIntPtr);
     funcNode->setArg(2, stackPointerReg);
+
+    // type-specific packing
+    vector<asmjit::x86::Gp> gps;
+    
+    gps.push_back(compiler.newGp(kUint64));
+    compiler.mov(gps[0], asmjit::x86::ptr(stackPointer, 0));
+    // TYPE CHECKING
+
 }
 
 // Return type is always Slot. _this is always Slot. int32_t is the thing we need, and we need a pointer to _BasicNew
